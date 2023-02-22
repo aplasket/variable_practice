3a58f2d3154ec31fa2a32b4f3061f3e9d62ffdc5
Ashley Plasket <ashley.plasket@gmail.com>
2023-02-22 15:48:47 -0500

    Assign new values to string variables

--- variables.rb
+++ variables.rb
@@ -15,3 +15,15 @@ months_in_ga = 5.5
 is_a_student = true
 has_job = false
 loves_turing = true
+
+# Reassign string variables
+name = "plaskey"
+last_name = "nothing"
+location = "hawaii"
+
+puts name
+puts location
+puts last_name
+puts account_balance
+puts has_job
+puts age
